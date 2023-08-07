069dab71102c533af6e8df74f536b9d947e9221d
Caioscg <cacazaok5@gmail.com>
2023-08-07 18:17:02 -0300

    nodemon

--- src/server.js
+++ src/server.js
@@ -1,9 +1,25 @@
 require("express-async-errors")
 
+const AppError = require("./utils/AppError")
+
 const express = require("express")
 
 const app = express()
 app.use(express.json())
+
+app.use(( error, req, res, next ) => {
+    if (error instanceof AppError) {
+        return res.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+    }
+
+    return res.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    })
+})
 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
