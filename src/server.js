05658f7e3abfa7d63c48873b86ddfa887250596b
Caioscg <cacazaok5@gmail.com>
2023-08-08 20:18:51 -0300

    C:/Program Files/Git/routes

--- src/server.js
+++ src/server.js
@@ -4,8 +4,12 @@ const AppError = require("./utils/AppError")
 
 const express = require("express")
 
+const routes = require("./routes")
+
 const app = express()
 app.use(express.json())
+
+app.use(routes)
 
 app.use(( error, req, res, next ) => {
     if (error instanceof AppError) {
