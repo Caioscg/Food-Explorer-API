c1450cc78183d7e7219dca911e4aea2f451ed187
Caioscg <cacazaok5@gmail.com>
2023-08-15 23:42:20 -0300

    handling errors

--- src/controller/MealsController.js
+++ src/controller/MealsController.js
@@ -4,6 +4,15 @@ class MealsController {
     async create(req, res) {
         const { name, category, ingredients, price, description } = req.body
 
+        if (!name) throw new AppError("O nome é obrigatório!")
+
+        if (!category) throw new AppError("Adicione uma categoria ao prato!")
+
+        if (!ingredients) throw new AppError("Os ingredientes são obrigatórios!")
+
+        if (!price) throw new AppError("O preço é obrigatório!")
+
+        if (!description) throw new AppError("Adicione uma descrição ao prato!")
 
         return res.status(201).json()
     }
